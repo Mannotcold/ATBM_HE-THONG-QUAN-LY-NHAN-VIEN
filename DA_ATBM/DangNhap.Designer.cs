2a19a5610014b540951f3a6006fe25acc3fef872
Mannotcold <man2082002@gmail.com>
2023-04-18 10:01:13 +0700

    Update UI DangNhap

--- DA_ATBM/DangNhap.Designer.cs
+++ DA_ATBM/DangNhap.Designer.cs
@@ -29,12 +29,116 @@ namespace DA_ATBM
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxShowPass = new System.Windows.Forms.CheckBox();
+            this.textBoxMK = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTK = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(213, 233);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Hiển thị MK";
+            // 
+            // checkBoxShowPass
+            // 
+            this.checkBoxShowPass.AutoSize = true;
+            this.checkBoxShowPass.Location = new System.Drawing.Point(282, 233);
+            this.checkBoxShowPass.Name = "checkBoxShowPass";
+            this.checkBoxShowPass.Size = new System.Drawing.Size(15, 14);
+            this.checkBoxShowPass.TabIndex = 15;
+            this.checkBoxShowPass.UseVisualStyleBackColor = true;
+            // 
+            // textBoxMK
+            // 
+            this.textBoxMK.Location = new System.Drawing.Point(153, 207);
+            this.textBoxMK.Name = "textBoxMK";
+            this.textBoxMK.Size = new System.Drawing.Size(143, 20);
+            this.textBoxMK.TabIndex = 14;
+            this.textBoxMK.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(78, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(302, 25);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Quản lý thông tin nhân viên";
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(182, 272);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 12;
+            this.buttonLogin.Text = "Đăng nhập";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(150, 190);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Tài khoản";
+            // 
+            // textBoxTK
+            // 
+            this.textBoxTK.Location = new System.Drawing.Point(153, 146);
+            this.textBoxTK.Name = "textBoxTK";
+            this.textBoxTK.Size = new System.Drawing.Size(143, 20);
+            this.textBoxTK.TabIndex = 9;
+            // 
+            // DangNhap
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(460, 364);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBoxShowPass);
+            this.Controls.Add(this.textBoxMK);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxTK);
+            this.Name = "DangNhap";
             this.Text = "DangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxShowPass;
+        private System.Windows.Forms.TextBox textBoxMK;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTK;
     }
 }
